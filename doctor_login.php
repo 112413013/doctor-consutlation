d9c13d88da8b7abda88fe2186482200778b09680
112413013 <112413013@kanchiuniv.ac.in>
2026-09-09 07:13:47 +0530

    Create doctor_login.php

---
+++ doctor_login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Doctor Login</title>
+</head>
+
+<body>
+
+    <h1>Doctor Login</h1>
+
+    <form method="POST" action="doctor_login.php">
+
+        <label>Email</label>
+        <br>
+        <input type="email" name="email" placeholder="Enter your email" required>
+
+        <br><br>
+
+        <label>Password</label>
+        <br>
+        <input type="password" name="password" placeholder="Enter your password" required>
+
+        <br><br>
+
+        <button type="submit">Login</button>
+
+    </form>
+
+</body>
+
+</html>
